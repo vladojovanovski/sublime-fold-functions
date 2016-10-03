71f73b7f456cd6df95c04a101d0a6d96d67bd9ae
Hernan Zhou <hernanzh@gmail.com>
2016-10-03 16:26:07 +0200

    cleanup, settings

--- foldfunctions.py
+++ foldfunctions.py
@@ -1,44 +1,32 @@
 import sublime, sublime_plugin
 
-# class MyListener(sublime_plugin.EventListener):
-#     openBraces = []
-#     closedBraces = []
-#     def on_selection_modified_async(self, view):
-#         # print ("cursor", view.sel()[0])
-#         braces = view.find_by_selector('meta.brace.curly.js')
-#         hasUnfolded = False
-        
-#         self.openBraces = []
-#         self.closedBraces = []
+settings = {}
 
-#         for brace in braces:
-#             braceStr = view.substr(brace)
-#             if (braceStr == "{"):
-#                 self.openBraces.append(brace.a)
-#             elif braceStr == "}":
-#                 region = sublime.Region(self.openBraces.pop(), brace.b)
-#                 self.closedBraces.append(region)
+def init_plugin():
+    global settings
 
-#         for brace in self.closedBraces:
-#             left = sublime.Region(brace.a - 2, brace.a + 1)
-#             right = sublime.Region(brace.b + 0, brace.b + 1)
-#             leftStr = view.substr(left)
-#             rightStr = view.substr(right)
-#             if (leftStr == ") {" and (rightStr == ";" or rightStr == ",")):
-#                 inner = sublime.Region(brace.a + 1, brace.b - 1);
-#                 hasCursor = brace.intersects(view.sel()[0]);
-#                 if hasCursor and not hasUnfolded:
-#                     hasUnfolded = True
-#                     view.unfold(sublime.Region(brace.a + 1, brace.b - 1))
-#                 elif not hasCursor:
-#                     hasUnfolded = False
-#                     view.fold(inner)
+    # Setup settings
+    settings = sublime.load_settings('foldfunctions.sublime-settings')
+
+    # I guess these reload on settings change?
+    settings.clear_on_change('reload')
+    settings.add_on_change('reload', init_plugin)
+
+def plugin_loaded():
+    init_plugin()
 
 class CursorListener(sublime_plugin.EventListener):
+    # this listener checks if a folded region has been selected
     openBraces = []
     closedBraces = []
     def on_selection_modified_async(self, view):
-        # print ("cursor", view.sel()[0])
+        unfold = bool(settings.get("select_to_unfold", False))
+        syntax = view.settings().get('syntax');
+        # syntax should be Packages/JavaScript/JavaScript.sublime-syntax
+        if (not unfold or not ("JavaScript" in syntax)):
+            return
+
+        # print("cursor", view.sel()[0])
         braces = view.find_by_selector('meta.brace.curly.js')
         hasUnfolded = False
         
@@ -73,6 +61,10 @@ class FoldFunctionsCommand(sublime_plugin.TextCommand):
         self.openBraces = []
         self.closedBraces = []
 
+        closeConstructors = bool(settings.get("fold_constructors", False))
+
+        # unfortunately sublime text doesn't have selectors for starting and closing braces
+        # gonna have to search for the opening and closing braces myself
         for brace in braces:
             braceStr = view.substr(brace)
             if (braceStr == "{"):
@@ -82,9 +74,17 @@ class FoldFunctionsCommand(sublime_plugin.TextCommand):
                 self.closedBraces.append(region)
 
         for brace in self.closedBraces:
+            # check if the brace regions are suitable for closing
+            # look at the characters left of the opening brace
             left = sublime.Region(brace.a - 2, brace.a + 1)
             leftStr = view.substr(left)
-            hasCursor = brace.intersects(view.sel()[0]);
+            leftNoSpac = sublime.Region(brace.a - 1, brace.a + 1)
+            leftNoSpacStr = view.substr(leftNoSpac)
+            # the cursor should not be here
+            sels = self.view.sel()
+            hasCursor = False
+            for sel in sels:
+                hasCursor = hasCursor or brace.intersects(sel);
             # left of brace.a should be parameter list
             hasParameters = False
             for param in parameters:
@@ -93,11 +93,13 @@ class FoldFunctionsCommand(sublime_plugin.TextCommand):
                     break
             # also close constructors...?
             hasConstructor = False
-            for constructor in constructors:
-                if left.intersects(constructor):
-                    hasConstructor = True
-                    break
+            if closeConstructors:
+                for constructor in constructors:
+                    if left.intersects(constructor):
+                        hasConstructor = True
+                        break
             if (not hasCursor):
-                if ((leftStr == ") {" and hasParameters) or hasConstructor):
+                # there might or might not be a space -> function () {} vs function (){}
+                if ((leftStr == ") {" and hasParameters) or (leftNoSpacStr == "){" and hasParameters) or hasConstructor):
                     inner = sublime.Region(brace.a + 1, brace.b - 1);
                     view.fold(inner)
